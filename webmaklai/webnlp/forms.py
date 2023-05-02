ea722b0cdf5793c3bdff1a03f80ec666181202ef
PrabhuUdurg <egorpeshiy@gmail.com>
2023-05-02 18:54:37 +0200

    widget

--- webmaklai/webnlp/forms.py
+++ webmaklai/webnlp/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 
 class InputForm(forms.Form):
-    post = forms.CharField(label="Text", max_length=1000)
+    post = forms.CharField(label="Text", max_length=1000,
+                           widget=forms.Textarea(attrs={'rows': 5}))
